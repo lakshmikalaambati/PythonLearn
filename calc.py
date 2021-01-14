686e1d9cc3267f25cdf6cad9bbc9dc65c49a614c
Lakshmi kalavathi <56877693+lakshmikalaambati@users.noreply.github.com>
2021-01-14 23:01:48 +0100

    Started learning Python

---
+++ calc.py
@@ -0,0 +1,8 @@
+#Asking user to enter first number
+x = int(input('Enter First number '))
+#asking user to enter second number
+y = int(input('Enter Second Number '))
+#Adding given two numbers
+result=x+y
+#printing added result
+print(result)
